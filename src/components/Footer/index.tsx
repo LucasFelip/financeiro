f39e09177e2ee0b392a7f147aae0688851569120
Lucas Ferreira <luscafreis@gmail.com>
2023-03-05 14:57:08 -0300

    initial project

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <footer className="py-2 my-4">
+            <div className="nav justify-content-center border-bottom pb-3 mb-3">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/LucasFelip" target="_blank" rel="noreferrer">Lucas Felipe</a></p>
+            </div>
+            <p className="text-center text-muted">© 2023 Lucas Ferreira</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,17 @@
+
+const Home = () => {
+    return (
+        <>
+            <div className="container">
+                <div className="jumbotron">
+                    <h1 className="display-4">Financereiro</h1>
+                    <p className="lead">Analise de ganhos e custos de um individuo</p>
+                    <hr />
+                    <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Home;
